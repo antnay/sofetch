805c13e649c596a195d68f7bd6a43361254df30b
Anthony <149122963+antnay@users.noreply.github.com>
2024-05-13 11:53:08 -0700

    Delete target directory

--- target/release/deps/sofetch-4a748f62f46fe339.d
+++
@@ -1,5 +0,0 @@
-/Users/anthony/programming/rust/sofetch/target/release/deps/sofetch-4a748f62f46fe339: src/main.rs
-
-/Users/anthony/programming/rust/sofetch/target/release/deps/sofetch-4a748f62f46fe339.d: src/main.rs
-
-src/main.rs:
--- target/release/sofetch.d
+++
@@ -1,1 +0,0 @@
-/Users/anthony/programming/rust/sofetch/target/release/sofetch: /Users/anthony/programming/rust/sofetch/src/main.rs
